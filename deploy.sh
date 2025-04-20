22ba4448c5843a6332f7accac8414f2d159bee49
VepuriGnanesh <108175234+VepuriGnanesh@users.noreply.github.com>
2025-04-20 13:28:06 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
-=======
 This update for cherry-pick...
->>>>>>> main
 This file is updated in master branch 
 This file is updated in dev branch
 this is a updated line..
 this is a untracked file in the working area
+this is a pull request.....
